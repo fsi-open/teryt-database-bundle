82fb63744af6d5af3d54c75854536068e830d602
Lukasz Cybula <lukasz@fsi.pl>
2019-03-19 15:49:56 +0100

    Removed deprecated method

--- Behat/Context/Console/ApplicationTester.php
+++ Behat/Context/Console/ApplicationTester.php
@@ -15,7 +15,6 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Output\StreamOutput;
 
@@ -55,10 +54,6 @@ class ApplicationTester
 
         $this->initializeInputStream();
         rewind($this->inputStream);
-
-        /** @var QuestionHelper $questionHelper */
-        $questionHelper = $this->application->getHelperSet()->get('question');
-        $questionHelper->setInputStream($this->inputStream);
 
         return $this->application->doRun($this->input, $this->output);
     }
